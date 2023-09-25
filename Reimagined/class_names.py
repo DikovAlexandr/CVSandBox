5efdffb9a40a54adbacf4809034154b037b65329
Диков Александр <112902848+DikovAlexandr@users.noreply.github.com>
2023-09-25 20:17:30 +0300

    Class names correction

--- Reimagined/class_names.py
+++ Reimagined/class_names.py
@@ -1,10 +1,6 @@
 # List of instance categories with their names.
 INSTANCE_CATEGORY_NAMES = [
     '__background__', 
-    'divider-line',
-    'dotted-line',
-    'double-line',
-    'random-line',
-    'road-sign-line',
-    'solid-line'
+    'table',
+    'plot'
 ]
