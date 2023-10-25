1b7be9eff9ab15f58cc0a4cd2fffc3dd0d9b556f
Диков Александр <112902848+DikovAlexandr@users.noreply.github.com>
2023-10-25 19:16:05 +0300

    Some symbols deleted from lines

--- tesseract_tutorial/split_training_text.py
+++ tesseract_tutorial/split_training_text.py
@@ -27,39 +27,47 @@ if not os.path.exists(output_directory):
 
 random.shuffle(lines)
 
-count = 1000
+count = 2000
 
 lines = lines[:count]
 
 line_count = 0
 for line in lines:
-    num_lines = random.randint(1, 4)
-    multi_line_text = line #re.sub(r' ', '\n', line, num_lines)
+    f = False
+    for symbol in ['#', '№', '$', '&', '@,' '€', '₽', 'ё', 'Ё']:
+        if symbol in line:
+            f = True
+    
+    if f == False:
+        num_lines = random.randint(1, 2)
+        multi_line_text = re.sub(r' ', '\n', line, num_lines)
 
-    training_text_file_name = pathlib.Path(training_text_file).stem
-    print (training_text_file_name)
-    line_training_text = os.path.join(output_directory, f'{training_text_file_name}_{line_count}.gt.txt')
-    with open(line_training_text, 'w', encoding='utf-8') as output_file:
-        output_file.writelines([line])
+        print(line, '\n', num_lines, multi_line_text, '\n')
 
-    file_base_name = f'rus_{line_count}'
+        training_text_file_name = pathlib.Path(training_text_file).stem
+        # print (training_text_file_name)
+        line_training_text = os.path.join(output_directory, f'{training_text_file_name}_{line_count}.gt.txt')
+        with open(line_training_text, 'w', encoding='utf-8') as output_file:
+            output_file.writelines([multi_line_text])
 
-    subprocess.run([
-        'text2image',
-        '--font=Literaturnaya', # Literaturnaya
-        f'--text={line_training_text}',
-        f'--outputbase={output_directory}/{file_base_name}',
-        '--max_pages=1',
-        '--degrade_image=true',
-        '--strip_unrenderable_words',
-        '--leading=10',
-        '--xsize=1000',
-        '--ysize=100',
-        '--margin=10',
-        '--ptsize=6',
-        '--char_spacing=0.0',
-        '--exposure=0',
-        '--unicharset_file=langdata/rus.unicharset'
-    ])
+        file_base_name = f'rus_{line_count}'
 
-    line_count += 1
+        subprocess.run([
+            'text2image',
+            '--font=Literaturnaya', # Literaturnaya
+            f'--text={line_training_text}',
+            f'--outputbase={output_directory}/{file_base_name}',
+            '--max_pages=1',
+            '--degrade_image=true',
+            '--strip_unrenderable_words',
+            '--leading=10',
+            '--xsize=1000',
+            '--ysize=150',
+            '--margin=10',
+            '--ptsize=6',
+            '--char_spacing=0.0',
+            '--exposure=0',
+            '--unicharset_file=langdata/rus.unicharset'
+        ])
+
+        line_count += 1
